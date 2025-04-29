34e424b8821a14cc63501de1639b15a3b154e699
gydnjs <khw33352182@gmail.com>
2025-04-29 20:56:47 +0900

    refactor: resourceManager, Define
    Release하는 부분을 Unload와 UnloadAll로 바꾸었다.
    Addressable의 라벨 값과 키 값의 계층 구조를 만들어 좀 더 유지보수가 쉽게 바꿈

--- Assets/@Resource/Script/Manager/ResourceManager.cs
+++ Assets/@Resource/Script/Manager/ResourceManager.cs
@@ -1,7 +1,7 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using Cysharp.Threading.Tasks;
-using UnityEditor.AddressableAssets.Settings;
 using UnityEngine;
 using UnityEngine.AddressableAssets;
 using UnityEngine.ResourceManagement.AsyncOperations;
@@ -22,86 +22,113 @@ public class ResourceManager
         return null;
     }
 
+    public (string[],T[]) LoadAll<T>(Define.AddressableLabelGroup labelGroup) where T : UnityEngine.Object
+    {
+        List<T> objs = new List<T>();
+        List<string> keys = new List<string>();
+        string[] dictKeys = resources.Keys.ToArray<string>();
+        foreach (string key in resources.Keys.ToArray())
+        {
+            if (key.StartsWith(labelGroup.ToString()+"/"))
+            {
+                objs.Add(Load<T>(key));
+                keys.Add(key);
+            }
+        }
+        return (keys.ToArray<string>(),objs.ToArray<T>());
+    }
+
     public async UniTask<T> LoadAsync<T>(string key) where T : UnityEngine.Object
     {
+        if (resources.ContainsKey(key))
+            return resources[key] as T;
+
         AsyncOperationHandle<T> op = Addressables.LoadAssetAsync<T>(key);
 
         //비동기 작업이 끝날때까지 대기함
         await op.Task;
+        if (op.Status == AsyncOperationStatus.Succeeded)
+        {
+            T obj = op.Result;
+            resources.Add(key, obj as UnityEngine.Object);
 
-        try
-        {
-            if (op.Status == AsyncOperationStatus.Succeeded)
-            {
-                T obj = op.Result;
-                resources.Add(key, obj as UnityEngine.Object);
-                
-                return obj;
-            }
-            else
-            {
-                Debug.LogError($"Addressable 동기 로딩 실패 키값 : {key}");
-                return null;
-            }
+            return obj;
         }
-        finally
+        else
         {
-            Addressables.Release(op);
+            Debug.LogError($"Addressable 동기 로딩 실패 키값 : {key}");
+            return null;
         }
     }
 
     public void LoadAsync<T>(string key, Action<T> action = null) where T : UnityEngine.Object
     {
         AsyncOperationHandle<T> op = Addressables.LoadAssetAsync<T>(key);
-        op.Completed += (bojca) =>
+        op.Completed += (loadedObject) =>
         {
-            try
+            if (op.Status == AsyncOperationStatus.Succeeded)
             {
-                if (op.Status == AsyncOperationStatus.Succeeded)
-                {
-                    T obj = op.Result;
-                    resources.Add(key, obj as UnityEngine.Object);
-                    action?.Invoke(obj);
-                }
-                else
-                    Debug.LogError($"Addressable 비동기 로딩 실패 키값 : {key}");
+                T obj = op.Result;
+                resources.Add(key, obj as UnityEngine.Object);
+                action?.Invoke(obj);
             }
-
-            finally
-            {
-                Addressables.Release(op);
-            }
+            else
+                Debug.LogError($"Addressable 비동기 로딩 실패 키값 : {key}");
         };
     }
 
-    public async UniTask LoadAllAsync<T>(string label) where T : UnityEngine.Object
+    public async UniTask<T[]> LoadAllAsync<T>(string label) where T : UnityEngine.Object
     {
         AsyncOperationHandle<IList<IResourceLocation>> op = Addressables.LoadResourceLocationsAsync(label, typeof(T));
 
         await op.Task;
-
-        try
+        if (op.Status == AsyncOperationStatus.Succeeded)
         {
-            if (op.Status == AsyncOperationStatus.Succeeded)
+            T[] objs = new T[op.Result.Count];
+            int index = 0;
+            foreach (IResourceLocation item in op.Result)
             {
-                foreach(IResourceLocation item in op.Result)
-                {
-                    await LoadAsync<T>(item.PrimaryKey);
-                }
+                T obj = await LoadAsync<T>(item.PrimaryKey);
+                objs[index] = obj;
+                index++;
             }
-            else
-            {
-                Debug.Log($"LoadAllAsync 실패 라벨값 {label}");
-            }
+            return objs;
         }
-        finally
-        {
-            Addressables.Release(op);
-        }
+        else
+            Debug.Log($"LoadAllAsync 실패 라벨값 {label}");
+        return null;
+        
     }
-    public void LoadAllAsync<T>(string label) where T : UnityEngine.Object
+    public void LoadAllAsync<T>(string label, Action<UnityEngine.Object> callback) where T : UnityEngine.Object
     {
+        AsyncOperationHandle<IList<IResourceLocation>> op = Addressables.LoadResourceLocationsAsync(label, typeof(T));
 
+        op.Completed += (loadedObject) =>
+        {
+            IList<IResourceLocation> keys = loadedObject.Result;
+            foreach(IResourceLocation item in keys)
+            {
+                string key = item.PrimaryKey;
+                LoadAsync<T>(key, callback);
+            }
+        };
+    }
+
+    public void UnLoad<T>(string key) where T : UnityEngine.Object
+    {
+        T obj = Load<T>(key);
+        Addressables.Release<T>(obj);
+        resources.Remove(key);
+    }
+    public void UnLoadAll<T>(Define.AddressableLabelGroup label) where T : UnityEngine.Object
+    {
+        (string[] ,T[]) objs = LoadAll<T>(label);
+
+        for (int i = 0; i < objs.Item1.Length; i++)
+        {
+            Addressables.Release(objs.Item2[i]);
+            resources.Remove(objs.Item1[i]);
+        }
     }
 }
 
