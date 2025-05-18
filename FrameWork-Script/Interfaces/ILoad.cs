3705144d1e96919021aaa982d441f0a2572279a8
hyowonKim <khw33352182@gmail.com>
2025-05-19 01:22:19 +0900

    refactor: ILoad now inherits IDependency interface
    ILoad가 IDependency를 상속 받게 수정

--- FrameWork-Script/Interfaces/ILoad.cs
+++ FrameWork-Script/Interfaces/ILoad.cs
@@ -1,20 +1,20 @@
 using Cysharp.Threading.Tasks;
-using NUnit.Framework;
 using System;
 using System.Collections.Generic;
 using UnityEngine;
 
-public interface ILoad : IPool 
+public interface ILoad : IPool, IDependency 
 {
     public T Load<T>(string key) where T : UnityEngine.Object;
 
 }
 
-public interface ILoadAsync : IPool
+public interface ILoadAsync : IPool, IDependency
 {
-    public T LoadAsync<T>(string key) where T : UnityEngine.Object;
-
-    public UniTask<List<T>> LoadAllAsync<T>(string key) where T : UnityEngine.Object;
+    public UniTask<T> LoadAsync<T>(string key) where T : UnityEngine.Object;
+    public void LoadAsync<T>(string key, Action<T> callback = null) where T : UnityEngine.Object;
+    public UniTask<T[]> LoadAllAsync<T>(string label) where T : UnityEngine.Object;
+    public void LoadAllAsync<T>(string label, Action<T> callback = null) where T : UnityEngine.Object;
 }
 
 public class Loader : ILoad
@@ -33,4 +33,53 @@ public class Loader : ILoad
     public T Load<T>(string key) where T : UnityEngine.Object
     {
         return resourceManager.Load<T>(key);
-    }}
+    }
+}
+public class AsyncLoader : ILoadAsync
+{
+    ResourceManager resourceManager;
+    public void Init()
+    {
+        resourceManager = new ResourceManager();
+    }
+
+    public async UniTask<T[]> LoadAllAsync<T>(string label) where T : UnityEngine.Object
+    {
+        T[] loadedObjects = await resourceManager.LoadAllAsync<T>(label);
+
+        if (loadedObjects == null)
+        {
+            Debug.Log($"{label} 라벨 값을 가진 오브젝트가 존재하지 않음");
+            return null;
+        }
+
+        return loadedObjects;
+    }
+
+    public void LoadAllAsync<T>(string label, Action<T> callback = null) where T : UnityEngine.Object
+    {
+        resourceManager.LoadAllAsync<T>(label, callback);
+    }
+
+    public async UniTask<T> LoadAsync<T>(string key) where T : UnityEngine.Object
+    {
+        T loadedObject = await resourceManager.LoadAsync<T>(key);
+        if (loadedObject == null)
+        {
+            Debug.Log($"{key} 키값은 가진 오브젝트가 존재하지 않습니다.");
+            return null;
+        }
+        return loadedObject;
+    }
+
+    public void LoadAsync<T>(string key, Action<T> callback = null) where T : UnityEngine.Object
+    {
+        resourceManager.LoadAsync(key, callback);
+    }
+
+    public void Pool()
+    {
+        DIContainer.ReturnInstance(this);
+        resourceManager = null;
+    }
+}
